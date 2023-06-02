642d9617569e92b35d79e5452a33df1672b4112f
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-06-02 16:01:29 -0300

    make the rainbow feature

--- js.js
+++ js.js
@@ -13,18 +13,32 @@ const generateButton = document.querySelector('#optionsContainer button').addEve
 
 
 function paint(e){
-if(e.buttons == 1){
+if(e.buttons == 1 && e.target != EASContainer){
 	e.target.classList.remove('notPainted');
 	e.target.classList.add('painted');
+  e.target.style.cssText = '';
 }
 }
 
 function unPaint(e){
-if(e.buttons == 1){
+if(e.buttons == 1 && e.target != EASContainer){
 	e.target.classList.remove('painted');
 	e.target.classList.add('notPainted');
+  e.target.style.cssText = '';
 }
 }
+
+
+let randomNumber;
+function paintRainbow(e){
+randomNumber = Math.round(Math.random() * 360);
+if(e.buttons == 1 && e.target != EASContainer){
+e.target.style.cssText= `background-color:hsl(${randomNumber}, 100%, 50%)`;
+}
+
+}
+
+
 
 const clearButton =document.querySelectorAll('#optionsContainer2 button')[0];
 clearButton.addEventListener('click', clearTheDrawing);
@@ -35,6 +49,10 @@ eraseButton.addEventListener('click', setToEraseMode);
 const paintButton = document.querySelectorAll('#optionsContainer2 button')[2];
 paintButton.addEventListener('click', setToPaintMode);
 
+
+const rainbowButton = document.querySelectorAll('#optionsContainer2 button')[3];
+rainbowButton.addEventListener('click', setToRainbowMode);
+
 function clearTheDrawing(){
 
 const EASContainerItems = document.querySelectorAll('#EASContainer div');
@@ -43,26 +61,45 @@ if(EASContainerItems[index].className == 'painted'){
 EASContainerItems[index].classList.remove('painted');
 EASContainerItems[index].classList.add('notPainted');
 }
+EASContainerItems[index].style.cssText = "";
 }
 }
 
 function setToPaintMode(){
 mode = 'paint';
+paintButton.classList.add('paintMode');
+eraseButton.classList.remove('eraseMode');
+rainbowButton.classList.remove('rainbowMode');
 }
 
 function setToEraseMode(){
 mode = 'erase';
+paintButton.classList.remove('paintMode');
+eraseButton.classList.add('eraseMode');
+rainbowButton.classList.remove('rainbowMode');
+}
+
+function setToRainbowMode(){
+mode = 'rainbow';
+paintButton.classList.remove('paintMode');
+eraseButton.classList.remove('eraseMode');
+rainbowButton.classList.add('rainbowMode');
+
 }
 
 
 function actionOverDrawing(e){
 if(mode == 'paint'){
 paint(e);}
-else{
+else if(mode == 'erase'){
 unPaint(e);
+}
+else{
+paintRainbow(e);
 }
 
 }
+
 
 
 

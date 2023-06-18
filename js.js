17a41ab3fde290b0e56b5fb07ac1896e61bdd81a
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-06-18 18:36:36 -0300

    add the hability of add cuztomized color

--- js.js
+++ js.js
@@ -4,11 +4,71 @@ const slice = document.querySelector('#optionsContainer input');
 slice.addEventListener('mousemove', updateSliceSizeText);
 
 function updateSliceSizeText(){
-document.querySelector('#optionsContainer h1 ').innerText = `size: ${slice.value}x${slice.value}`;
+document.querySelector('#optionsContainer h2 ').innerText = `size: ${slice.value}x${slice.value}`;
 }
 
 
+
+let optionsContainer3 = document.querySelector('#optionsContainer3');
+
+let optionsContainerInputs = document.querySelectorAll('#optionsContainer3 input');
+let optionsContainerSpans = document.querySelectorAll('#optionsContainer3 h2 span');
+
+let smallDiv = document.querySelector('#smallSquare')
+
+document.addEventListener('mousemove',updateOptionContainerAndColor)
+function updateOptionContainerAndColor(){
+
+if(mode == "color"){
+optionsContainer3.style.display = 'flex';
+}
+else{
+optionsContainer3.style.display = 'none';
+}
+
+optionsContainerSpans[0].innerText = optionsContainerInputs[0].value;
+optionsContainerSpans[2].innerText = optionsContainerInputs[1].value;
+optionsContainerSpans[4].innerText = optionsContainerInputs[2].value;
+
+smallDiv.style.backgroundColor = `hsl(${optionsContainerInputs[0].value}, ${optionsContainerInputs[1].value}%, ${optionsContainerInputs[2].value}%)`;
+}
+
+
+
+
 const generateButton = document.querySelector('#optionsContainer button').addEventListener('click', generateEtchASketchWithSizeInMind)
+
+
+const EASContainer = document.querySelector('#EASContainer');
+
+function generateEtchASketchWithSizeInMind(){
+clearTheDrawing();
+document.querySelector('footer').style.cssText = 'display: flex'
+document.querySelector('#optionsContainer2').style.cssText = 'display: flex';
+EASContainer.style.cssText = `display: grid; grid-template-columns: repeat(${slice.value},1fr); grid-template-rows: repeat(${slice.value},1fr); `;
+for(let index = 0; Number(slice.value * slice.value) > index; index++){
+div = document.createElement('div');
+div.id = `n${index}`;
+div.classList.add('notPainted');
+div.ondragstart = function () {return false};
+EASContainer.appendChild(div);
+}
+EASContainer.addEventListener('mouseover',actionOverDrawing);
+}
+
+
+
+function clearTheDrawing(){
+
+const EASContainerItems = document.querySelectorAll('#EASContainer div');
+for(let index = 0;EASContainerItems.length > index;index++){
+if(EASContainerItems[index].className == 'painted'){
+EASContainerItems[index].classList.remove('painted');
+EASContainerItems[index].classList.add('notPainted');
+}
+EASContainerItems[index].style.cssText = "";
+}
+}
 
 
 
@@ -39,6 +99,15 @@ e.target.style.cssText= `background-color:hsl(${randomNumber}, 100%, 50%)`;
 }
 
 
+//todo
+function paintCustomColor(e){
+if(e.buttons == 1 && e.target != EASContainer){
+e.target.style.backgroundColor = `hsl(${optionsContainerInputs[0].value}, ${optionsContainerInputs[1].value}%, ${optionsContainerInputs[2].value}%)`
+}
+
+}
+
+
 
 const clearButton =document.querySelectorAll('#optionsContainer2 button')[0];
 clearButton.addEventListener('click', clearTheDrawing);
@@ -53,22 +122,16 @@ paintButton.addEventListener('click', setToPaintMode);
 const rainbowButton = document.querySelectorAll('#optionsContainer2 button')[3];
 rainbowButton.addEventListener('click', setToRainbowMode);
 
-function clearTheDrawing(){
 
-const EASContainerItems = document.querySelectorAll('#EASContainer div');
-for(let index = 0;EASContainerItems.length > index;index++){
-if(EASContainerItems[index].className == 'painted'){
-EASContainerItems[index].classList.remove('painted');
-EASContainerItems[index].classList.add('notPainted');
-}
-EASContainerItems[index].style.cssText = "";
-}
-}
+const colorButton = document.querySelectorAll('#optionsContainer2 button')[4];
+colorButton.addEventListener('click', setToColorMode);
+
 
 function setToPaintMode(){
 mode = 'paint';
 paintButton.classList.add('paintMode');
 eraseButton.classList.remove('eraseMode');
+colorButton.classList.remove('colorMode');
 rainbowButton.classList.remove('rainbowMode');
 }
 
@@ -76,6 +139,7 @@ function setToEraseMode(){
 mode = 'erase';
 paintButton.classList.remove('paintMode');
 eraseButton.classList.add('eraseMode');
+colorButton.classList.remove('colorMode');
 rainbowButton.classList.remove('rainbowMode');
 }
 
@@ -83,8 +147,16 @@ function setToRainbowMode(){
 mode = 'rainbow';
 paintButton.classList.remove('paintMode');
 eraseButton.classList.remove('eraseMode');
+colorButton.classList.remove('colorMode');
 rainbowButton.classList.add('rainbowMode');
+}
 
+function setToColorMode(){
+mode = 'color';
+paintButton.classList.remove('paintMode');
+eraseButton.classList.remove('eraseMode');
+rainbowButton.classList.remove('rainbowMode');
+colorButton.classList.add('colorMode');
 }
 
 
@@ -94,9 +166,12 @@ paint(e);}
 else if(mode == 'erase'){
 unPaint(e);
 }
-else{
+else if(mode == 'rainbow'){
 paintRainbow(e);
 }
+else{
+paintCustomColor(e);
+}
 
 }
 
@@ -104,22 +179,4 @@ paintRainbow(e);
 
 
 
-const EASContainer = document.querySelector('#EASContainer');
-
-function generateEtchASketchWithSizeInMind(){
-clearTheDrawing();
-document.querySelector('footer').style.cssText = 'display: flex'
-document.querySelector('#optionsContainer2').style.cssText = 'display: flex';
-EASContainer.style.cssText = `display: grid; grid-template-columns: repeat(${slice.value},1fr); grid-template-rows: repeat(${slice.value},1fr); `;
-
-for(let index = 0; Number(slice.value * slice.value) > index; index++){
-div = document.createElement('div');
-div.id = `n${index}`;
-div.classList.add('notPainted');
-div.ondragstart = function () {return false};
-EASContainer.appendChild(div);
-}
-
-EASContainer.addEventListener('mouseover',actionOverDrawing);
-}
 
